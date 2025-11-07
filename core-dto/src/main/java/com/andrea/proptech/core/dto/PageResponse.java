907562decf5b8470f4f8e25b3afe1996ca142b61
Andrea Donadello <andreadonadello@protonmail.com>
2025-11-07 17:02:59 +0100

    Refactor page response

--- core-dto/src/main/java/com/andrea/proptech/core/dto/PageResponse.java
+++ core-dto/src/main/java/com/andrea/proptech/core/dto/PageResponse.java
@@ -1,27 +1,40 @@
 package com.andrea.proptech.core.dto;
 
+import lombok.Builder;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+@Builder
 public record PageResponse<T>(
         List<T> content,
-        int currentPage,
-        int size,
-        long totalElements,
-        int totalPages,
-        boolean first,
-        boolean last
+        PageMetadata page
 ) {
+
+    @Builder
+    public record PageMetadata(
+            int currentPage,
+            int size,
+            long totalElements,
+            int totalPages,
+            boolean first,
+            boolean last
+    ) {
+    }
+
     public static <T> PageResponse<T> fromPage(Page<T> page) {
-        return new PageResponse<>(
-                page.getContent(),
-                page.getNumber() + 1,
-                page.getSize(),
-                page.getTotalElements(),
-                page.getTotalPages(),
-                page.isFirst(),
-                page.isLast()
-        );
+        PageMetadata pageMetadata = PageMetadata.builder()
+                .currentPage(page.getNumber() + 1)
+                .size(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .first(page.isFirst())
+                .last(page.isLast())
+                .build();
+
+        return PageResponse.<T>builder()
+                .content(page.getContent())
+                .page(pageMetadata)
+                .build();
     }
 }
